146ce80468c2d6599c2e693b7641e52312db461b
T-tssxuan <2270360114@qq.com>
2016-10-10 19:00:42 +0800

    [alter] change the wifi_ap preprocess function

--- code/tools.py
+++ code/tools.py
@@ -3,7 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-def for_wifi_ap():
+def get_data():
+    # Read data from csv file and process the data to a approprioate format
+    # 
     wifi_ap = pd.read_csv('./data/WIFI_AP_Passenger_Records_chusai_1stround.csv')
     fmt = '%Y-%m-%d-%H-%M-%S'
     wifi_ap['timeStamp'] = pd.to_datetime(wifi_ap['timeStamp'], format=fmt)
@@ -18,7 +20,15 @@ def for_wifi_ap():
             ap.groupby(pd.TimeGrouper(granularity)).aggregate(np.sum) 
             for ap in new_wifi_ap
             ]
+    return new_wifi_ap
+
+
+def for_wifi_ap():
+    data = get_data()
     plt.figure()
-    for ele in new_wifi_ap:
+    for ele in data:
         ele.plot()
     plt.show()
+    
+def for_deviation_calculate():
+    data = get_data()
