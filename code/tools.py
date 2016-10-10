e0fb3a3e948881b8ce59e7062a08dbdbf0afb6a8
T-tssxuan <2270360114@qq.com>
2016-10-11 00:26:01 +0800

    [alter] new

--- code/tools.py
+++ code/tools.py
@@ -3,15 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-def get_data():
-    # Read data from csv file and process the data to a approprioate format
-    # 
+# Read data from csv file and process the data to an appropriate format
+def ap_get_data_group_by_ap(gran=10):
     wifi_ap = pd.read_csv('./data/WIFI_AP_Passenger_Records_chusai_1stround.csv')
     fmt = '%Y-%m-%d-%H-%M-%S'
     wifi_ap['timeStamp'] = pd.to_datetime(wifi_ap['timeStamp'], format=fmt)
     wifi_ap = wifi_ap.set_index('WIFIAPTag')
     wifi_ap = wifi_ap.groupby(wifi_ap.index)
-    granularity = '10Min'
+    granularity = gran + 'Min'
     new_wifi_ap = [
             wifi_ap.get_group(key).set_index('timeStamp')
             for key in wifi_ap.groups.keys()
@@ -23,12 +22,15 @@ def get_data():
     return new_wifi_ap
 
 
-def for_wifi_ap():
-    data = get_data()
+# Plat the ap access information for given granularity
+def ap_plot_data_info(gran=10):
+    data = ap_get_data_group_by_ap(gran)
     plt.figure()
     for ele in data:
         ele.plot()
     plt.show()
     
-def for_deviation_calculate():
-    data = get_data()
+# Generate the data in the difference format with given granularity
+def ap_get_data_by_difference(gran=10):
+    data = ap_get_data_group_by_ap(gran)
+
