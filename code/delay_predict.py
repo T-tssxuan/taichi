0d55f9f85accb9d3c836e04e272b37679de2bc79
T-tssxuan <2270360114@qq.com>
2016-10-14 16:21:19 +0800

    [finish] the delay predict module

--- code/delay_predict.py
+++ code/delay_predict.py
@@ -9,34 +9,78 @@ class PredictDelay:
     Predict the delay time for a plane
     '''
     def __init__(self):
-        self.X, self.y = self.get_data()
+        self.csv = pd.read_csv('./data/airport_gz_flights_chusai_1stround.csv')
+        del self.data['flight_ID']
+        del self.data['BGATE_ID']
+        self.data.columns = ['sch', 'act']
 
-    def train(self):
-        params = {'n_estimators': 1000, 'max_depth': 5, 'max_feature': 'sqrt'}
-        clf = gbdt(params)
-        rst = cross_val_score(clf, self.X, self.y)
-        print(rst)
-
-    def get_data(self):
-        data = pd.read_csv('./data/airport_gz_flights_chusai_1stround.csv')
-        del data['flight_ID']
-        del data['BGATE_ID']
-        data.columns = ['sch', 'act']
-
-        data = data[pd.notnull(data['act'])]
         data['act'] = pd.to_datetime(data['act'])
-
-        data = data[pd.notnull(data['sch'])]
         data['sch'] = pd.to_datetime(data['sch'])
-        
-        data = data.sort_values('sch')
 
+        data = data.sort_values('sch')
         data = data.reset_index()
         del data['index']
+        
+    def train(self):
+        X, y = self.get_data()
 
-        # func1 = lambda x: data[data['sch'] >= x['sch']][data['sch'] <= x['act']]
-        # data['sch_overlap'] = data['sch'].apply(func1)
+        self.mean = y.mean()
+        self.std = y.std()
 
+        self.clf = gbdt(n_estimators=1000, max_depth=5, max_features='sqrt')
+        rst = cross_val_score(self.clf, X, y)
+        print(rst)
+        info = 'result: ' + str(rst.mean()) + '% confidence and with ';
+        info += str(rst.std()) + ' accuracy.'
+        print(info)
+
+        self.clf.fit(self.X, self.y)
+
+    def predict(self, start, end):
+        start = pd.to_datetime(start)
+        end = pd.to_datetime(end)
+
+        X, _ = get_predict_data(start, end)
+        pdelay = self.clf.predict(X)
+
+        return get_predict_result(start, end, pdelay)
+
+    def get_predict_result(self, rstart, rend, rdelay):
+        rdata = self.csv.copy()
+
+        rdelay = rdelay.apply(lambda x: pd.DateOffset(minutes=x))
+
+        offset = pd.DateOffset(sself.mean + 2.7 * self.std)
+        rdata = rdata[rdata['sch'] > rstart - offset]
+        rdata = rdata[rdata['sch'] < rend + offset]
+
+        rdata['act'] = rdata['sch'].add(rdelay)
+
+        rdata = rdata[rdata['act'] > rstart]
+
+        return rdata['act']
+
+    def get_predict_data(self, pstart, pend):
+        pdata = self.csv.copy()
+
+        offset = pd.DateOffset(sself.mean + 2.7 * self.std)
+        pdata = pdata[pdata['sch'] > pstart - offset]
+        pdata = pdata[pdata['sch'] < pend + offset]
+
+        # get the random delay and then bind it the plane we need to predict
+        ran_delay = np.random.normal(self.mean, self.std, len(pdata.shape[0]))
+        ran_delay = pd.Series(ran_delay)
+        pdata['act'] = pdata['sch'].add(ran_delay)
+        return preprocess_data(data, offset)
+
+    def get_train_data(self):
+        tdata = self.csv.copy()
+        return preprocess_data(tdata, 100)
+
+    def preprocess_data(self, data, delay_treshold):
+        data = data[pd.notnull(data['act'])]
+        data = data[pd.notnull(data['sch'])]
+        
         sch_overlap = {}
         for idx, row in data.iterrows():
             tmp = data[data['sch'] >= row['sch']]
@@ -45,9 +89,6 @@ class PredictDelay:
         sch_overlap = pd.DataFrame.from_dict(sch_overlap, 'index')
         sch_overlap.columns = ['sch_overlap']
         
-        # func1 = lambda x: data[data['act'] >= x['sch']][data['act'] <= x['act']]
-        # data['act_overlap'] = data['sch'].apply(func1)
-
         act_overlap = {}
         for idx, row in data.iterrows():
             tmp = data[data['act'] >= row['sch']]
@@ -59,9 +100,9 @@ class PredictDelay:
         data = pd.concat([data, sch_overlap], axis=1)
         data = pd.concat([data, act_overlap], axis=1)
 
-        func = lambda x: x.total_seconds()
+        func = lambda x: x.total_seconds() / 60
         data['delay'] = data['act'].subtract(data['sch']).apply(func)
-        data = data[data['delay'] < 400]
+        data = data[data['delay'] < delay_treshold]
         delay = data['delay']
         del data['delay']
 
